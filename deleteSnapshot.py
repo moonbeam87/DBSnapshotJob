3eec57f062b87ab730c4541e45d0c80e82d46271
Dev <2021dpatel@gmail.com>
2020-12-16 11:33:40 -0500

    updated after testing

--- deleteSnapshot.py
+++ deleteSnapshot.py
@@ -1,18 +1,31 @@
+import json
 import boto3
-from datetime import datetime, timedelta
+import datetime
+from datetime import timedelta
+
 def lambda_handler(event, context):
     print("Connecting to RDS")
     client = boto3.client('rds')
     dbInstances = ['mysql-dev']
     for dbInstance in dbInstances:
+        print(dbInstance)
+        #response1 = client.describe_db_instances(
+        #    DBInstanceIdentifier=dbInstance    
+        #)
+        #print(response1)
         print("RDS stale snapshot deletion started at %s...\n" % datetime.datetime.now())
         response = client.describe_db_snapshots(
             DBInstanceIdentifier=dbInstance
+            #DBSnapshotIdentifier = 'mysql-dev'
         )
-        for i in response('DBSnapshots'):
+        print(response)
+        for i in response['DBSnapshots']:
+            print(i)
             delta = datetime.now()-i('SnapshotCreateTime')
+            print("Time Delta:")
+            print(delta)
             if delta >= (365*2):
-                t = i('DBSnapshotIdentifier')
+                t = i['DBSnapshotIdentifier']
                 client.delete_db_snapshot(
                     DBSnapshotIdentifier=t
                 )
