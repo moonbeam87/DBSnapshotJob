8204e1d884e91ee3278989427bfa869b63f649a9
Dev <2021dpatel@gmail.com>
2020-12-16 14:39:03 -0500

    fixed after testing

--- deleteSnapshot.py
+++ deleteSnapshot.py
@@ -1,31 +1,22 @@
 import json
 import boto3
-from datetime import timedelta
+
 import datetime
+from datetime import timedelta
 
 def lambda_handler(event, context):
     print("Connecting to RDS")
     client = boto3.client('rds')
-    dbInstances = ['mysql-dev']
-    for dbInstance in dbInstances:
-        print(dbInstance)
-        #response1 = client.describe_db_instances(
-        #    DBInstanceIdentifier=dbInstance    
-        #)
-        #print(response1)
-        print("RDS stale snapshot deletion started at %s...\n" % datetime.datetime.now())
-        response = client.describe_db_snapshots(
-            DBInstanceIdentifier=dbInstance
-            #DBSnapshotIdentifier = 'mysql-dev'
-        )
-        print(response)
-        for i in response['DBSnapshots']:
-            print(i)
-            delta = datetime.date.today()-i['SnapshotCreateTime']
-            print("Time Delta:")
-            print(delta)
-            if delta >= (365*2):
-                t = i['DBSnapshotIdentifier']
-                client.delete_db_snapshot(
-                    DBSnapshotIdentifier=t
-                )
+    response = client.describe_db_snapshots()
+    for i in response['DBSnapshots']:
+        print(i)
+        delta = datetime.datetime.today()-datetime.timedelta(days=730)
+        print("Time Delta:")
+        print(delta)
+        delta = delta.replace(tzinfo=None)
+        snaptime = i['SnapshotCreateTime'].replace(tzinfo=None)
+        if snaptime < delta:
+            t = i['DBSnapshotIdentifier']
+            client.delete_db_snapshot(
+                DBSnapshotIdentifier=t
+            )
--- deleteSnapshotCluster.py
+++ deleteSnapshotCluster.py
@@ -1,38 +1,22 @@
 import json
 import boto3
+
 import datetime
 from datetime import timedelta
 
 def lambda_handler(event, context):
     print("Connecting to RDS")
     client = boto3.client('rds')
-    dbClusters = ['mysql-dev']
-    for dbCluster in dbClusters:
-        clusters = client.describe_db_clusters(
-            DBClusterIdentifier = dbCluster
-        )
-        for cluster in clusters['DBClusters']:
-            dbInstances = cluster['DBClusterMembers']
-            for dbInstance in dbInstances:
-                print(dbInstance['DBInstanceIdentifier'])
-                #response1 = client.describe_db_instances(
-                #    DBInstanceIdentifier=dbInstance    
-                #)
-                #print(response1)
-                dbInstance = dbInstance['DBInstanceIdentifier']
-                print("RDS stale snapshot deletion started at %s...\n" % datetime.datetime.now())
-                response = client.describe_db_snapshots(
-                    DBInstanceIdentifier=dbInstance
-                    #DBSnapshotIdentifier = 'mysql-dev'
-                )
-                print(response)
-                for i in response['DBSnapshots']:
-                    print(i)
-                    delta = datetime.date.today()-i['SnapshotCreateTime']
-                    print("Time Delta:")
-                    print(delta)
-                    if delta >= (365*2):
-                        t = i['DBSnapshotIdentifier']
-                        client.delete_db_snapshot(
-                            DBSnapshotIdentifier=t
-                        )
+    response = client.describe_db_cluster_snapshots()
+    for i in response['DBClusterSnapshots']:
+        print(i)
+        delta = datetime.datetime.today()-datetime.timedelta(days=730)
+        print("Time Delta:")
+        print(delta)
+        delta = delta.replace(tzinfo=None)
+        snaptime = i['SnapshotCreateTime'].replace(tzinfo=None)
+        if snaptime < delta:
+            t = i['DBSnapshotIdentifier']
+            client.delete_db_snapshot(
+                DBSnapshotIdentifier=t
+            )
